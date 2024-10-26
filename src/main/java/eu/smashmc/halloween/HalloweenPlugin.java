ff15d08165629bd1a6c75e3253c49b0eb41cec7c
Packsolite <packsolite@gmail.com>
2024-10-26 19:42:32 +0200

    :fire: Remove debug logs

--- src/main/java/eu/smashmc/halloween/HalloweenPlugin.java
+++ src/main/java/eu/smashmc/halloween/HalloweenPlugin.java
@@ -2,12 +2,17 @@ package eu.smashmc.halloween;
 
 import eu.smashmc.api.core.packet.PacketUtil;
 import eu.smashmc.lib.bukkit.world.location.Locations;
+import net.minecraft.server.v1_8_R3.EnumDifficulty;
 import net.minecraft.server.v1_8_R3.PacketPlayOutRespawn;
+import net.minecraft.server.v1_8_R3.WorldSettings;
+import net.minecraft.server.v1_8_R3.WorldType;
 import org.apache.commons.lang.reflect.FieldUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.block.Biome;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -16,9 +21,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class HalloweenPlugin extends JavaPlugin implements Listener {
 
+	private static final int DIMENSION = -1;
+
 	@Override
 	public void onEnable() {
-		setNether(Locations.spawn());
+		Bukkit.getScheduler().runTaskLater(this, () -> {
+			setNether(Locations.spawn());
+		}, 50);
 		Bukkit.getPluginManager().registerEvents(this, this);
 		PacketUtil.listenPacket(PacketPlayOutRespawn.class, event -> {
 			PacketPlayOutRespawn respawn = event.getPacket();
@@ -26,7 +35,7 @@ public class HalloweenPlugin extends JavaPlugin implements Listener {
 				return;
 			}
 			try {
-				FieldUtils.writeField(respawn, "a", -1, true);
+				FieldUtils.writeField(respawn, "a", DIMENSION, true);
 				event.setPacket(respawn);
 			} catch (IllegalAccessException e) {
 				throw new RuntimeException(e);
@@ -46,13 +55,20 @@ public class HalloweenPlugin extends JavaPlugin implements Listener {
 	@EventHandler
 	public void onJOin(PlayerJoinEvent event) {
 		var player = event.getPlayer();
-		Chunk chunk = player.getLocation()
-				.getChunk();
+		if (!player.getWorld().equals(Locations.spawn().getWorld())) {
+			return;
+		}
+		setDimension(player, DIMENSION);
+	}
+
+	public void setDimension(Player player, int dimension) {
+		CraftPlayer cp = (CraftPlayer) player;
+		PacketPlayOutRespawn packet = new PacketPlayOutRespawn(dimension, EnumDifficulty.EASY, WorldType.CUSTOMIZED, WorldSettings.EnumGamemode.ADVENTURE);
+		(cp.getHandle()).playerConnection.sendPacket(packet);
+		Chunk chunk = player.getLocation().getChunk();
 		for (int x = -10; x < 10; x++) {
-			for (int z = -10; z < 10; z++) {
-				player.getWorld()
-						.refreshChunk(chunk.getX() + x, chunk.getZ() + z);
-			}
+			for (int z = -10; z < 10; z++)
+				player.getWorld().refreshChunk(chunk.getX() + x, chunk.getZ() + z);
 		}
 	}
 }
